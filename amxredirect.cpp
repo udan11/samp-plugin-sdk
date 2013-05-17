0e229e51357c72e1f5a849164b95860350a29306
udan1107 <udan1107@gmail.com>
2013-05-17 22:49:52 +0300

    Added AMX table definitions and function for redirecting natives.

---
+++ amxredirect.cpp
@@ -0,0 +1,28 @@
+//----------------------------------------------------------
+//
+//   SA-MP Multiplayer Modification For GTA:SA
+//   Copyright 2013 SA-MP Team, Dan
+//
+//----------------------------------------------------------
+
+#include "amxredirect.h"
+
+//----------------------------------------------------------
+
+void redirect(AMX *amx, char *from, ucell to, AMX_NATIVE *store) {
+	AMX_HEADER *hdr = (AMX_HEADER*) amx->base;
+	AMX_FUNCSTUB *func;
+	for (int idx = 0, num = NUMENTRIES(hdr, natives, libraries); idx != num; ++idx) {
+		func = GETENTRY(hdr, natives, idx);
+		if (!strcmp(from, GETENTRYNAME(hdr, func))) {
+			if (store) {
+				*store = (AMX_NATIVE) func->address;
+			}
+			func->address = to;
+			return;
+		}
+	}
+}
+
+//----------------------------------------------------------
+// EOF
---
+++ amxtable.h
@@ -0,0 +1,31 @@
+//----------------------------------------------------------
+//
+//   SA-MP Multiplayer Modification For GTA:SA
+//   Copyright 2013 SA-MP Team, Dan
+//
+//----------------------------------------------------------
+
+#pragma once
+
+//----------------------------------------------------------
+
+#include "amx/amx.h"
+
+//----------------------------------------------------------
+
+#define USENAMETABLE(hdr) \
+	((hdr)->defsize==sizeof(AMX_FUNCSTUBNT))
+
+#define NUMENTRIES(hdr,field,nextfield) \
+	(unsigned)(((hdr)->nextfield - (hdr)->field) / (hdr)->defsize)
+
+#define GETENTRY(hdr,table,index) \
+	(AMX_FUNCSTUB *)((unsigned char*)(hdr) + (unsigned)(hdr)->table + (unsigned)index*(hdr)->defsize)
+
+#define GETENTRYNAME(hdr,entry) \
+	(USENAMETABLE(hdr) ? \
+		(char *)((unsigned char*)(hdr) + (unsigned)((AMX_FUNCSTUBNT*)(entry))->nameofs) : \
+		((AMX_FUNCSTUB*)(entry))->name)
+
+//----------------------------------------------------------
+// EOF
